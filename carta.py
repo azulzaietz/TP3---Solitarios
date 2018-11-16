66dc93c16ade065901eb8fdbaa84eb8370103f1c
enzomatiaslagos <39174935+enzomatiaslagos@users.noreply.github.com>
2018-11-16 01:31:11 -0300

    Update carta.py

--- carta.py
+++ carta.py
@@ -1,92 +1,133 @@
-from carta import Carta
-from carta import criterio, MISMO_PALO
+# Estas 3 constantes definen cómo se van a mostrar las cartas. Definirlas
+# según lo más adecuado a la plataforma. (Como modo "compatibilidad" definir
+# UNICODE_LINDO y COLOR en False y VALOR_10 en '10'.)
+UNICODE_LINDO = False
+VALOR_10 = '10' # Posibles variantes '⒑', '⏨', '10'
+COLOR = True
 
-class SolitarioError(Exception):
-    """Tipo de Exception para todos los errores del Solitario."""
-    # No tocar nada acá, el pass está bien como toda implementación :).
-    pass
+
+# Constantes que representan a los palos.
+PICAS = 0
+CORAZONES = 1
+DIAMANTES = 2
+TREBOLES = 3
 
 
-class PilaCartas:
-    """Representa una pila de cartas en el tablero."""
+# Constantes para criterios de comparación referidos a palos.
+MISMO_PALO = 0
+MISMO_COLOR = 1
+DISTINTO_COLOR = 2
+DISTINTO_PALO = 3
 
-    def __init__(self, pila_visible=False, valor_inicial=None, puede_desapilar=True, criterio_apilar=None, criterio_mover=None):
-        """Se construye una pila vacía. El comportamiento estará regido por:
-            pila_visible: Bool. ¿Al imprimir la pila se muestra sólo la carta
-                del tope o son visibles todas las cartas de la pila?
-            valor_inicial: Número 1 al 13. Si está presente sólo se puede
-                apilar ese valor sobre una pila vacía.
-            puede_desapilar: Bool. ¿En la pila se apila y se desapila o sólo
-                se puede apilar?
-            criterio_apilar: f(a, b). Si la pila no está vacía y tope() == a,
-                f(a, b) indica si puede apilarse la carta b sobre la pila.
-            criterio_mover: f(a, b). Siendo la pila una secuencia de cartas
-                [cn, ..., c2, c1, c0], donde c0 es el tope, voy a poder mover
-                en bloque parte de la pila siempre y cuando f(c1, c0),
-                f(c2, c1), etc. hasta que la función falle.
-        Todos los parámetros son optativos y tienen valores por omisión. En
-        el caso de los parámetros que sean None, los mismos no se
-        considerarán como restricciones (por ejemplo, si valor_inicial == None
-        se desactivará el chequeo de valor_inicial)."""
-        self.items = []
-        self.pila_visible = pila_visible
-        self.valor_inicial = valor_inicial
-        self.puede_desapilar = puede_desapilar
-        self.criterio_apilar = criterio_apilar
-        self.criterio_mover = criterio_mover
+# Constantes para criterios de comparación referidos a valores.
+ASCENDENTE = 0
+DESCENDENTE = 1
+CONSECUTIVA = 2
 
-    def es_vacia(self):
-        """Indica si la pila se encuentra vacía."""
-        return len(self.items) == 0
 
-    def tope(self):
-        """Devuelve la carta tope de la pila.
-        Levanta SolitarioError en caso de error."""
-        if not self.es_vacia():
-            return self.items[-1]
-        raise SolitarioError("La pila esta vacia.")
+class Carta:
+    """
+        Clase que representa una carta de baraja francesa.
+        La clase posee tres atributos:
+            valor: Un número del 1 al 13.
+            palo: Un valor entre PICAS, CORAZONES, DIAMANTES y TREBOLES.
+            boca_abajo: Un bool, indica si la carta está boca abajo.
+    """
 
-    def apilar(self, carta, forzar=False):
-        """Apila una carta en la pila. Si forzar es True desactiva los chequeos
-        sobre el valor_inicial y el criterio_apilar.
-        Levanta SolitarioError en caso de no poder apilar."""
-        self.items.append(carta)
+    def __init__(self, valor, palo, boca_abajo=True):
+        """Crea una carta de un valor, palo y boca_abajo. Por omisión la carta
+        está boca abajo."""
+        self.valor = valor
+        self.palo = palo
+        self.boca_abajo = boca_abajo
 
-    def desapilar(self):
-        """Desapila una carta. Levanta SolitarioError en caso de no poder
-        desapilar."""
-        if self.es_vacia():
-            raise SolitarioError("No hay mas cartas para desapilar")
-        return self.items.pop()
-
-    def mover(self, origen):
-        """Siendo origen otra PilaCartas intenta mover un subpilón de cartas
-        de origen sobre la pila.
-        Las primera carta que se apile sobre la pila debe validar
-        criterio_apilar mientras que la cantidad de cartas máxima a mover
-        desde origen dependerá de criterio_mover.
-        Independientemente del criterio_mover no podrán moverse cartas que se
-        encuentren boca abajo y sobre una carta boca abajo puede apilarse
-        cualquier valor.
-        Debe levantarse SolitarioError en caso de no poder mover ninguna carta
-        de origen a la pila."""
-        pass
+    def voltear(self):
+        """Da vuelta una carta."""
+        self.boca_abajo = not self.boca_abajo
 
     def __str__(self):
-        """Devuelve una representación de la pila.
-        La misma será una X si la pila estuviera vacía.
-        Si pila_visible == True se representará a la pila como todas las
-        cartas de base a tope separadas por espacios. Si no sólo se
-        representará según el tope."""
-        if self.es_vacia():
-            return "X"
-        elif self.pila_visible:
-            mostrar =""
-            for elemento in self.items:
-                mostrar += str(elemento)
-            return mostrar
-        return str(self.tope())
+        return _c2s(self.valor, self.palo) if not self.boca_abajo else _c2s(0, 0)
 
     def __repr__(self):
-        """Ídem __str__."""
-        pass
+        return str(self)
+
+    def __eq__(self, other):
+        return not self.boca_abajo and self.palo == other.palo and self.valor == other.valor
+
+
+def criterio(palo=None, orden=None):
+    """Generador de funciones de comparación de cartas.
+        palo: Un valor entre MISMO_PALO, MISMO_COLOR, DISTINTO_PALO, DISTINTO_COLOR.
+        orden: Un valor entre ASCENDENTE, DESCENDENTE o CONSECUTIVA.
+    Devuelve una función de comparación cmp(a, b) que indica si la carta b es apilable
+    sobre la carta a según el criterio indicado."""
+    def comp(a, b):
+        if a.boca_abajo or b.boca_abajo:
+            # Las cartas tienen que estar boca arriba.
+            return False
+
+        if orden is not None:
+            # Si hay restricción de valor:
+            if orden == ASCENDENTE:
+                if a.valor != b.valor + 1:
+                    return False
+            elif orden == DESCENDENTE:
+                if a.valor + 1 != b.valor:
+                    return False
+            if orden == CONSECUTIVA:
+                if a.valor % 13 + 1 != b.valor and a.valor != b.valor % 13 + 1:
+                    return False
+
+        if palo is not None:
+            # Si hay restricción de palo:
+            if palo == MISMO_PALO:
+                if a.palo != b.palo:
+                    return False
+            elif palo == MISMO_COLOR:
+                if (a.palo in (CORAZONES, DIAMANTES) and b.palo not in (CORAZONES, DIAMANTES)) or \
+                        (a.palo in (PICAS, TREBOLES) and b.palo not in (PICAS, TREBOLES)):
+                    return False
+            elif palo == DISTINTO_PALO:
+                if a.palo == b.palo:
+                    return False
+            elif palo == DISTINTO_COLOR:
+                if (a.palo in (CORAZONES, DIAMANTES) and b.palo in (CORAZONES, DIAMANTES)) or \
+                        (a.palo in (PICAS, TREBOLES) and b.palo in (PICAS, TREBOLES)):
+                    return False
+
+        return True
+    return comp
+
+
+# A partir de acá son funciones de bajo nivel de las rutinas de impresión...
+
+ANSI_ROJO = "\u001b[31;47m"
+ANSI_NEGRO = "\u001b[30;47m"
+ANSI_RESET = "\u001b[0m"
+
+def _c2s(valor, palo):
+    if COLOR:
+        if palo in (CORAZONES, DIAMANTES):
+            return _rojo(__c2s(valor, palo))
+        else:
+            return _negro(__c2s(valor, palo))
+    return __c2s(valor, palo)
+
+def _negro(s):
+    return ANSI_NEGRO + s + ANSI_RESET
+
+def _rojo(s):
+    return ANSI_ROJO + s + ANSI_RESET
+
+def __c2s(valor, palo):
+    if UNICODE_LINDO:
+        if valor > 11: valor += 1
+        return chr(0x1F0A0 + valor + palo*16)
+    else:
+        if not valor:
+            return '▓'
+        valores = ['A', '2', '3', '4', '5', '6', '7', '8', '9', VALOR_10, 'J', 'Q', 'K']
+        palos = "♠♥♦♣"
+
+        return valores[valor-1] + palos[palo]
+
